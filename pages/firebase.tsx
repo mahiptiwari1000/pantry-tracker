9c65c7a27b3180014afcb4d0bcfdf22be7b00aeb
unknown <mahiptiwari1000@gmail.com>
2024-08-14 21:36:06 -0500

    Fixed code

--- pages/firebase.tsx
+++ pages/firebase.tsx
@@ -6,6 +6,10 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+export default function Home1() {
+  return <></>;
+}
 const firebaseConfig = {
   apiKey: "AIzaSyBsMrW9kkKZBhwqc_xNaPwAyzUHltnuK6c",
   authDomain: "expense-tracker-app-e7c3d.firebaseapp.com",
